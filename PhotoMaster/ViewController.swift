4c512920fede45dc6710ddad3560df670737b980
Ryuma.h <ryuma810dragon@gmail.com>
2019-04-16 16:07:39 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by Ryuma Harada on 2019/04/16.
+//  Copyright © 2019 Ryuma Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
